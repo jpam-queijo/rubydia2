ff75d642261a1fb0aacd6285f0fa6876cf25ac82
jpam-queijo <jpam.queijo@gmail.com>
2025-02-01 17:00:47 -0300

    added generate and launch

--- src/bedrock_mod_generator.ts
+++ src/bedrock_mod_generator.ts
@@ -5,6 +5,8 @@ import { v4 as uuidv4 } from "uuid";
 import { modInfoToManifest, validateUUIDv4 } from "./bedrock_utils";
 import fs from "fs-extra";
 import path from "path";
+import os from "os";
+import open from "open";
 
 export class BedrockModGenerator extends BaseModGenerator {
     public static override generate(mod: Mod, path?: string): void {
@@ -24,8 +26,51 @@ export class BedrockModGenerator extends BaseModGenerator {
         this.generate(mod, path);
     }
 
+    public static generateResourcePackFromMod(mod: Mod, path: string): void {
+        console.log("[rubydia2] Generating resource pack...");
+        if (!path) {
+            path = "./build/resource_packs";
+        }
+
+        fs.ensureDirSync(path);
+
+        this.generateResourcePack(mod, this.generateOrGetUUIDs(), path);
+        console.log("[rubydia2] Done generating resource pack.");
+    }
+
+    public static generateBehaviorPackFromMod(mod: Mod, path: string): void {
+        console.log("[rubydia2] Generating behavior pack...");
+        if (!path) {
+            path = "./build/behavior_packs";
+        }
+
+        fs.ensureDirSync(path);
+
+        this.generateBehaviorPack(mod, this.generateOrGetUUIDs(), path);
+        console.log("[rubydia2] Done generating behavior pack.");
+    }
+
     public static override generateAndLaunch(mod: Mod): void {
-        // TODO
+        if (os.platform() === 'win32' && process.env.LOCALAPPDATA) {
+
+            // Getting Minecraft Bedrock Path on windows
+            let generate_path = path.join(process.env.LOCALAPPDATA,
+                 "Packages", "Microsoft.MinecraftUWP_8wekyb3d8bbwe",
+                  "LocalState", "games", "com.mojang");
+
+            // Generating mods
+            let rp_path = path.join(generate_path, "development_resource_packs", mod.modInfo.name);
+            let bp_path = path.join(generate_path, "development_behavior_packs", mod.modInfo.name);
+
+            this.generateResourcePackFromMod(mod, rp_path);
+            this.generateBehaviorPackFromMod(mod, bp_path);
+
+            // Launching Minecraft
+            console.log("[rubydia2] Done generating. launching Minecraft.");
+            open("minecraft://");
+        } else {
+            throw new Error("[rubydia2] Unsupported platform to launch game.");
+        }
     }
 
     public static generateMcAddon(mod: Mod, path?: string): void {
@@ -36,9 +81,9 @@ export class BedrockModGenerator extends BaseModGenerator {
         console.log("[rubydia2] Generating resource pack...");
         if (!generate_path) {
             generate_path = "./build/";
+            generate_path = path.join(generate_path, 'resource_packs');
         }
 
-        generate_path = path.join(generate_path, 'resource_packs');
 
         fs.ensureDirSync(generate_path);
 
@@ -51,9 +96,9 @@ export class BedrockModGenerator extends BaseModGenerator {
         console.log("[rubydia2] Generating behavior pack...");
         if (!generate_path) {
             generate_path = "./build/";
+            generate_path = path.join(generate_path, 'behavior_packs');
         }
 
-        generate_path = path.join(generate_path, 'behavior_packs');
 
         fs.ensureDirSync(generate_path);
 
@@ -81,7 +126,7 @@ export class BedrockModGenerator extends BaseModGenerator {
             { envName: 'RP_UUID_1', packType: 'resource_pack', index: 0},
             { envName: 'RP_UUID_2', packType: 'resource_pack', index: 1},
             { envName: 'BP_UUID_1', packType: 'behavior_pack', index: 0},
-            { envName: 'BP_UUID_1', packType: 'behavior_pack', index: 1}
+            { envName: 'BP_UUID_2', packType: 'behavior_pack', index: 1}
         ]
 
         for (const config of uuidConfigs) {
